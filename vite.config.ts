ba9072a337851b11ea6b6e66d2a6e7154ea07dbd
dedsechack-1337 <43165412+dedsechack-1337@users.noreply.github.com>
2026-06-10 13:39:24 +0530

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
 export default defineConfig({
-  base: "/PhoenixSIEM/",
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+  plugins: [react()],
+  base: '/PhoenixSIEM/'
+})
